7df3e777e4f964acc731c6ecb8113193bf2e10b8
kavya478 <kummithakavya@gmail.com>
2025-02-21 13:47:15 +0530

    add file

--- output.tf
+++ output.tf
@@ -7,3 +7,21 @@
 #     value = aws_subnet.public
   
 # }
+
+output "vpc_id" {
+    value = aws_vpc.main.id
+  
+}
+
+output "public_subnets_id" {
+    value =  aws_subnet.public[*].id
+}
+
+output "private_subnets_id" {
+    value =  aws_subnet.private[*].id
+}
+
+output "database_subnets_id" {
+    value = aws_subnet.database[*].id
+    
+}
